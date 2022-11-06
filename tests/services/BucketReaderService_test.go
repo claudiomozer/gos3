fa0f9a82014eb7c631ebf9e1f0230440a38eeedb
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-11-06 19:19:31 -0300

    test: ensure BucketReaderService returns error if an invalid bucket is provided

--- tests/services/BucketReaderService_test.go
+++ tests/services/BucketReaderService_test.go
@@ -3,6 +3,7 @@ package servicestest
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/claudiomozer/gos3/src/services"
 )
 
@@ -13,5 +14,15 @@ func TestShouldReturnAnErrorIfInvalidClientIsProvided(t *testing.T) {
 
 	if err == nil {
 		t.Errorf("Should return an error if invalid client is given")
+	}
+}
+
+func TestShouldReturnAnErrorIfAnInvalidBucketIsGiven(t *testing.T) {
+	sut := services.NewBucketReaderService(&s3.Client{})
+
+	err := sut.Read("")
+
+	if err == nil {
+		t.Errorf("Should return an error if invalid bucket is given")
 	}
 }
