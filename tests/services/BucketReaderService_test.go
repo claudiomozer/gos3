87301006bb471b8faad0bafc83ddf07db274f7b4
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-11-06 21:12:07 -0300

    test: ensure BucketReaderService returns objects on success

--- tests/services/BucketReaderService_test.go
+++ tests/services/BucketReaderService_test.go
@@ -10,7 +10,7 @@ import (
 func TestShouldReturnAnErrorIfInvalidClientIsProvided(t *testing.T) {
 	sut := services.NewBucketReaderService(nil)
 
-	err := sut.Read("")
+	_, err := sut.Read("")
 
 	if err == nil {
 		t.Errorf("Should return an error if invalid client is given")
@@ -20,7 +20,7 @@ func TestShouldReturnAnErrorIfInvalidClientIsProvided(t *testing.T) {
 func TestShouldReturnAnErrorIfAnInvalidBucketIsGiven(t *testing.T) {
 	sut := services.NewBucketReaderService(&s3.Client{})
 
-	err := sut.Read("")
+	_, err := sut.Read("")
 
 	if err == nil {
 		t.Errorf("Should return an error if invalid bucket is given")
@@ -30,9 +30,27 @@ func TestShouldReturnAnErrorIfAnInvalidBucketIsGiven(t *testing.T) {
 func TestShouldReturnAnErrorIfClientReturnsError(t *testing.T) {
 	sut := services.NewBucketReaderService(&s3.Client{})
 
-	err := sut.Read("teste")
+	_, err := sut.Read("teste")
 
 	if err == nil {
 		t.Errorf("Should return an error if client returns error")
+	}
+}
+
+func TestShouldReturnObjectsOnSuccess(t *testing.T) {
+	config, err := services.NewAwsSdkConfigLoaderService().LoadAwsSdkConfig()
+	if err == nil {
+		client := services.NewGetS3ClientService(config).Get()
+		sut := services.NewBucketReaderService(client)
+
+		objects, err := sut.Read("datatracking-web")
+
+		if err != nil {
+			t.Errorf("Should not return an error on success")
+		}
+
+		if len(objects) == 0 {
+			t.Error("Should not return an empty array on success")
+		}
 	}
 }
